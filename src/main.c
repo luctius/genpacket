53c98da0219ebaf88599e87d4772b1a5b6e142dc
Cor Peters <corpeters@incas3.nl>
2014-11-26 15:11:30 +0100

    removed \n

--- src/main.c
+++ src/main.c
@@ -17,15 +17,12 @@ int main(int argc, char **argv) {
     (void) argc;
     (void) argv;
 
-
     yyparse();
-
     if (has_parse_error) {
         parse_error(NULL, NULL, "too many errors; quitting");
         exit(EXIT_FAILURE);
     }
 
-    printf("\n");
     for (int i = 0; i < packet_list_sz; i++) {
         printf("%s\n", packet_to_str(i) );
     }
